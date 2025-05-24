5c9844044db6a3a2f8397278691e6b63744ecf8a
jefritirta30 <jefritirta01@gmail.com>
2025-05-24 13:54:31 +0700

    jefri: edit frontend awal

---
+++ frontend-old/app/sewa/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SewaPage = () => {
+  return (
+    <div>
+      <h1>Form Sewa Kamera</h1>
+      <p>Isi form di bawah ini untuk menyewa kamera.</p>
+    </div>
+  );
+};
+
+export default SewaPage;
