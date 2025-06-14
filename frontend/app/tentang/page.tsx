9ac27fa57c30c2f4eed98e3c16997d1b4b351b27
jefritirta30 <jefritirta01@gmail.com>
2025-06-14 19:35:47 +0700

    jefri: membuat judul heading tentang kami

--- frontend/app/tentang/page.tsx
+++ frontend/app/tentang/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <main className="max-w-5xl mx-auto px-4 py-12">
+        <h1 className="text-3xl font-bold text-center mb-8">Tentang Kami</h1>
+    </main>
   )
 }
